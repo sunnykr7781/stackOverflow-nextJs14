6882c43d3a406e183ba86fe6cf7f99199bdb3e3c
sunny <sunny129271@gmail.com>
2024-07-24 15:30:12 +0530

    themesProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
 import "./globals.css"
 import { Inter, Space_Grotesk } from "next/font/google"
 import type { Metadata } from "next"
+import { ThemeProvider } from "@/context/ThemeProvider"
 const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -35,24 +36,25 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider  appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: 'primary-text-gradient hover:text-primary-500'
-      }
-    }}>
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-         
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
