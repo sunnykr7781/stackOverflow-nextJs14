47f9c40c2495c60dd8517f8276256dc68f5e3b43
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2024-10-17 11:57:14 +0530

    creating questions completed

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,14 +1,10 @@
-"use client"
-
 import Image from 'next/image'
-import React, { useEffect, useRef, useState } from 'react'
-import { Input } from '@/components/ui/input'
-
+import React from 'react'
+import { Input } from "@/components/ui/input"
 
 const GlobalSearch = () => {
-
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden" >
+    <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
         <Image 
           src="/assets/icons/search.svg"
@@ -19,13 +15,11 @@ const GlobalSearch = () => {
         />
 
         <Input
-         type="text"
+          type="text"
           placeholder="Search globally"
-                  className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
-
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
-   
     </div>
   )
 }
