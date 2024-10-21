37f6d9d1bd6fae9211e7f256e4463edc7566a50a
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2024-10-21 10:33:36 +0530

    fixed errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,9 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose"
-import {  } from "./shared.types"; 
 import { revalidatePath } from "next/cache";
-import { DeleteUserParams , CreateUserParams , UpdateUserParams} from "./shared.types";
+import { DeleteUserParams , CreateUserParams , UpdateUserParams} from "./shared.types"
 import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
