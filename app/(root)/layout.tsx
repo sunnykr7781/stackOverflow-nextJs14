1f35aeb0211e5f68308031f51d5d0acf2860be56
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2024-10-21 10:02:02 +0530

    updated deployement

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Navbar from "@/components/shared/navbar/Navbar"
 import LeftSidebar from "@/components/shared/LeftSidebar"
 
-import RightSidebar from "@/components/shared/rightSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
