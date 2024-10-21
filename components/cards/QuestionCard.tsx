f8251fabcb89851eb1d1eed2225e2564a7131089
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2024-10-21 11:31:47 +0530

    fixed error

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -51,7 +51,7 @@ const QuestionCard = ({
       
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
-          <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+          <RenderTag key={tag._id} _id={tag._id as string} name={tag.name} />
         ))}
       </div>
 
