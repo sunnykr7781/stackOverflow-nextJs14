996384977136224977cff9190dd4c907b6389810
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2024-10-21 08:57:52 +0530

    questionn section updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ const spaceGrotesk = Space_Grotesk({
 })
 
 export const metadata: Metadata = {
-  title: "ScriptFlow",
+  title: "ScriptOverflow",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             alt="Scriptflow"
           />
           <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-            Dev <span className="text-primary-500">Overflow</span>
+            Script<span className="text-primary-500">Overflow</span>
           </p>
         </Link>
         <GlobalSearch/>
