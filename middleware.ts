487500b9f86ef184cd874b9fca02b523e46950ff
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2024-11-12 03:33:06 +0530

    added public api webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,22 @@
 import { clerkMiddleware } from "@clerk/nextjs/server"
 
-export default clerkMiddleware()
+export default clerkMiddleware(
+  {
+    publicRoutes: [
+      '/',
+      '/api/webhook',
+      'question/:id',
+      '/tags',
+      '/tags/:id',
+      '/profile/:id',
+      '/community',
+      '/jobs'
+    ],
+    ignoredRoutes: [
+      '/api/webhook', '/api/chatgpt'
+    ]
+  }
+)
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
